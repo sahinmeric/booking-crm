13b5b7f5f41d7958e0937d07839e03837c0f2022
Sahin Meric <sahin.meric@medisprout.com>
2024-10-30 14:22:42 -0500

    Simple dashboard added

---
+++ frontend/src/routes/dashboard/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<Navbar />
+
+<main class="flex flex-col items-center p-4">
+	<h1 class="text-3xl font-bold mb-6">Dashboard</h1>
+</main>
