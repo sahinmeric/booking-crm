cc60c61565b1718621310585e904d6dbffd3b84a
Sahin Meric <sahin.meric@medisprout.com>
2024-10-30 17:39:51 -0500

    added tables

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -10,8 +10,8 @@
 
 <nav class="bg-gray-800 text-white p-4">
 	<ul class="flex space-x-4">
-		<li><button on:click={() => navigate('dashboard')}>Dashboard</button></li>
 		<li><button on:click={() => navigate('customers')}>Customers</button></li>
+		<li><button on:click={() => navigate('tables')}>Tables</button></li>
 		<li><button on:click={() => navigate('reservations')}>Reservations</button></li>
 	</ul>
 </nav>
