b79e1e293ffef1f864cbce761df384e74fc5fe4b
Sahin Meric <sahin.meric@medisprout.com>
2024-10-30 22:16:07 -0500

    logout button and funtionality added

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
+	import { goto } from '$app/navigation';
 
 	const dispatch = createEventDispatcher();
 
 	function navigate(view: string) {
 		dispatch('navigate', { view });
 	}
+
+	function logout() {
+		localStorage.removeItem('authToken');
+		goto('/login');
+	}
 </script>
 
-<nav class="bg-gray-800 text-white p-4">
-	<ul class="flex space-x-4">
-		<li><button on:click={() => navigate('customers')}>Customers</button></li>
-		<li><button on:click={() => navigate('tables')}>Tables</button></li>
-		<li><button on:click={() => navigate('reservations')}>Reservations</button></li>
-	</ul>
+<nav class="flex justify-between items-center p-4 bg-gray-800 text-white">
+	<div class="flex space-x-4">
+		<ul class="flex space-x-4">
+			<li><button on:click={() => navigate('customers')}>Customers</button></li>
+			<li><button on:click={() => navigate('tables')}>Tables</button></li>
+			<li><button on:click={() => navigate('reservations')}>Reservations</button></li>
+		</ul>
+	</div>
+	<button on:click={logout} class="bg-red-500 p-2 rounded hover:bg-red-600 transition">
+		Logout
+	</button>
 </nav>
